0a9964ffcc2c1367d80ec0073d2ac891e17efa16
Valtara Executive <vizzuettguerreroangel@gmail.com>
2026-06-27 13:07:32 -0600

    Update Capacitor config for plugins and remove SplashScreen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,24 +4,23 @@ const config: CapacitorConfig = {
   appId: 'com.valtara.proficha',
   appName: 'ProFicha',
   webDir: 'dist',
-  bundledWebRuntime: false,
   plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      launchAutoHide: true,
-      backgroundColor: "#ffffff",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
-    },
     CapacitorSQLite: {
+      iosDatabaseLocation: 'Library/CapacitorDatabase',
+      iosIsEncryption: false,
       androidIsEncryption: false,
       androidBiometric: {
         biometricAuth: false,
-        biometricTitle: "Verificación de acceso",
-        biometricSubTitle: "Inicie sesión para continuar"
+        biometricTitle : "Verificación de acceso",
+        biometricSubTitle : "Inicie sesión para continuar"
       }
+    },
+    GoogleAuth: {
+      scopes: ["profile", "email", "https://www.googleapis.com/auth/drive.file"],
+      serverClientId: "1099016850057-v232nk1h4nvnhtu01cf0j5kfan3t0884.apps.googleusercontent.com",
+      forceCodeForRefreshToken: true
     }
-  },
+  }
 };
 
 export default config;
