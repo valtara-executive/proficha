91f80c60c1c3b0706792d87a3e124428967c7d1f
Valtara Executive <vizzuettguerreroangel@gmail.com>
2026-06-26 18:10:50 -0600

    Add multiple new forms to index.js

--- src/forms/index.js
+++ src/forms/index.js
@@ -1,13 +1,46 @@
-import { formEntrenamiento } from './form_entrenamiento';
-import { formYoga } from './form_yoga';
-import { formArtesMarciales } from './form_artes_marciales';
-import { formVeterinaria } from './form_veterinaria';
-// ... Aquí importarás todos los demás conforme los vayas creando
+import form_medico from './form_medico';
+import form_psicologia from './form_psicologia';
+import form_nutricion from './form_nutricion';
+import form_fisioterapia from './form_fisioterapia';
+import form_odontologia from './form_odontologia';
+import form_enfermeria from './form_enfermeria';
+import form_optometria from './form_optometria';
+import form_podologia from './form_podologia';
+import form_pediatria from './form_pediatria';
+import form_logopedia from './form_logopedia';
+import form_masoterapia from './form_masoterapia';
+import form_acupuntura from './form_acupuntura';
+import form_quiropraxia from './form_quiropraxia';
+import form_reiki from './form_reiki';
+import form_coaching from './form_coaching';
+import form_terapia_pareja from './form_terapia_pareja';
+import form_soladora from './form_soladora';
+import form_psicopedagogia from './form_psicopedagogia';
+import form_doula from './form_doula';
+import form_manicura from './form_manicura';
+import form_cosmetologia from './form_cosmetologia';
+import form_depilacion from './form_depilacion';
+import form_med_estetica from './form_med_estetica';
+import form_maquillaje from './form_maquillaje';
+import form_tatuaje from './form_tatuaje';
+import form_piercing from './form_piercing';
+import form_microblading from './form_microblading';
+import form_micropigmentacion from './form_micropigmentacion';
+import form_entrenamiento from './form_entrenamiento';
+import form_yoga from './form_yoga';
+import form_artes_marciales from './form_artes_marciales';
+import form_veterinaria from './form_veterinaria';
 
 export const allForms = {
-  entrenamiento: formEntrenamiento,
-  yoga: formYoga,
-  artes_marciales: formArtesMarciales,
-  veterinaria: formVeterinaria,
-  // ... Aquí agregarás las llaves de los otros
+  medico: form_medico, psicologia: form_psicologia, nutricion: form_nutricion,
+  fisioterapia: form_fisioterapia, odontologia: form_odontologia, enfermeria: form_enfermeria,
+  optometria: form_optometria, podologia: form_podologia, pediatria: form_pediatria,
+  logopedia: form_logopedia, masoterapia: form_masoterapia, acupuntura: form_acupuntura,
+  quiropraxia: form_quiropraxia, reiki: form_reiki, coaching: form_coaching,
+  terapia_pareja: form_terapia_pareja, soladora: form_soladora, psicopedagogia: form_psicopedagogia,
+  doula: form_doula, manicura: form_manicura, cosmetologia: form_cosmetologia,
+  depilacion: form_depilacion, med_estetica: form_med_estetica, maquillaje: form_maquillaje,
+  tatuaje: form_tatuaje, piercing: form_piercing, microblading: form_microblading,
+  micropigmentacion: form_micropigmentacion, entrenamiento: form_entrenamiento,
+  yoga: form_yoga, artes_marciales: form_artes_marciales, veterinaria: form_veterinaria
 };
